1838b427cfadfd30d57c33a753c7be9ec805e17c
Alexandre Russi - Mac <alexandrerussijunior@gmail.com>
2019-09-26 22:53:19 -0300

    Voz Firebase

--- AI/voice_speech.py
+++ AI/voice_speech.py
@@ -2,6 +2,22 @@
 import pyttsx3
 import engineio
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('/Users/mac/Documents/GitHub/Autonomous-Car/AI/alphanumbraskem-firebase.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://alphanumbraskem.firebaseio.com/'
+})
+
+ref = db.reference('/profiles/funcionario1/nome')
+
+print(ref.get())
+
 engineio = pyttsx3.init() #inicializa a engine
 voices = engineio.getProperty('voices')
 
@@ -12,17 +28,17 @@ voices = engineio.getProperty('voices')
 #     print(voice.name)
 
 engineio.setProperty('rate', 130)    # Aquí puedes seleccionar la velocidad de la voz
-# engineio.setProperty('voice',voices[0].id) # escolhe a voz a ser falada
+#engineio.setProperty('voice',voices[0].id) # escolhe a voz a ser falada
 engineio.setProperty('voice', b'brazil') # passando a voz para ptbr
 
 def speak(text):
     engineio.say(text) # fala a string setada
     engineio.runAndWait() # executa e espera
 
-speak("Testando")
-while(1):
-    phrase = input("--> ")
-    if (phrase == "exit"):
-        exit(0)
-    speak(phrase) # seta a string a ser falada
-    print(voices)
+speak('Olá, ' + str(ref.get()) + '! Meu nome é Alpha!')
+# while(1):
+#     phrase = input("--> ")
+#     if (phrase == "exit"):
+#         exit(0)
+#     speak(phrase) # seta a string a ser falada
+#     print(voices)
